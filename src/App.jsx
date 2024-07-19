1c28d682a4985b303b2ca40a489d805ee614b4b8
Anmol Rahujo <128142834+anmol-rahujo@users.noreply.github.com>
2024-07-20 02:49:40 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -24,14 +24,14 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Anmol Rahujo",
+  title: "Technical Writer & Social Media Manager",
+  email: "anmolrahujoai@gmail.com",
+  gitHub: "anmol-rahujo",
+  instagram: "anmol_rahujo",
+  linkedIn: "anmol-rahujo",
   medium: "",
-  twitter: "microsoft",
+  twitter: "microsof",
   youTube: "Code",
 };
 
